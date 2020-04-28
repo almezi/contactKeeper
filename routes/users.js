ef78835cf97e831d17d705027a66db8fd1a5297a
unknown <auliameiazizi@gmail.com>
2020-04-28 15:56:48 +0700

    add hash password and register route

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
+const User = require('../models/Users');
 //@route POST aou/users
 //@desc  register a user
 //@access Public
-router.post('/', (req, res) => {
-	res.send('register a user');
-});
+router.post(
+	'/',
+	[
+		check('name', 'name is required').not().isEmpty(),
+		check('email', 'email is required').isEmail(),
+		check('password', 'password is must bigger than 6').isLength({ min: 6 }),
+	],
+	async (req, res) => {
+		const errors = validationResult(req);
+		if (!errors.isEmpty()) {
+			return res.status(400).json({ errors: errors.array() });
+		}
+		const { name, email, password } = req.body;
+		try {
+			let user = await User.findOne({ email: email });
+
+			if (user) {
+				return res.status(400).json({ msg: 'User already exists' });
+			}
+
+			user = new User({
+				name,
+				email,
+				password,
+			});
+
+			const salt = await bcrypt.genSalt(10);
+			user.password = await bcrypt.hash(password, salt);
+			await user.save();
+			res.send('user Saved');
+		} catch (err) {
+			console.error(err.massage);
+			res.status.send('server error');
+		}
+	}
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
+
+//init body parser middleware
+app.use(express.json({ extended: false }));
+//connect db
+connectDB();
 app.get('/', (req, res) => res.json({ msg: 'welcome to our API' }));
 //Define Routes
 app.use('/api/users', require('./routes/users'));
