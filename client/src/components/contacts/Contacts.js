64136f89501f8dbf6c326c2aa68f6e1d92fc0b9a
auliameiazizi@gmail.com <github_2sembilan>
2020-05-12 16:25:49 +0700

    Basic Add & delete animation

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,23 +1,30 @@
-import React, { Fragment, useContext } from 'react';
-import ContactContext from '../../Context/Contact/ContactContext';
-import ContactItem from './ContactItem';
+import React, { Fragment, useContext } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import ContactContext from "../../Context/Contact/ContactContext";
+import ContactItem from "./ContactItem";
 
 const Contacts = () => {
-	const contactContext = useContext(ContactContext);
-	const { contacts, filtered } = contactContext;
-	if (contacts.length == 0) {
-		return <h4>Please Add a contact</h4>;
-	}
-	return (
-		<Fragment>
-			{filtered !== null
-				? filtered.map((contact) => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))
-				: contacts.map((contact) => (
-						<ContactItem key={contact.id} contact={contact} />
-				  ))}
-		</Fragment>
-	);
+  const contactContext = useContext(ContactContext);
+  const { contacts, filtered } = contactContext;
+  if (contacts.length == 0) {
+    return <h4>Please Add a contact</h4>;
+  }
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames="item">
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
+    </Fragment>
+  );
 };
 export default Contacts;
