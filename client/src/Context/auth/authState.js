641f6e06b093118c961ee3eedc455c94b4423eb5
auliameiazizi@gmail.com <github_2sembilan>
2020-05-22 08:15:04 +0700

    add user registration

--- client/src/Context/auth/authState.js
+++ client/src/Context/auth/authState.js
@@ -1,45 +1,76 @@
-import React, { useReducer } from "react";
+import React, { useReducer } from 'react';
 
-import AuthContext from "./authContext";
-import AuthReducer from "./authReducer";
+import AuthContext from './authContext';
+import AuthReducer from './authReducer';
+import Axios from 'axios';
 import {
-  REGISTER_SUCCESS,
-  REGISTER_FAIL,
-  USER_LOADED,
-  AUTH_ERROR,
-  LOGIN_SUCCESS,
-  LOGIN_FAIL,
-  LOGOUT,
-  CLEAR_ERRORS,
-} from "../type";
+	REGISTER_SUCCESS,
+	REGISTER_FAIL,
+	USER_LOADED,
+	AUTH_ERROR,
+	LOGIN_SUCCESS,
+	LOGIN_FAIL,
+	LOGOUT,
+	CLEAR_ERRORS,
+} from '../type';
 
 const AuthState = (props) => {
-  const initialState = {
-    token: localStorage.getItem("token"),
-    isAuthenticated: null,
-    loading: true,
-    error: null,
-    user: null,
-  };
+	const initialState = {
+		token: localStorage.getItem('token'),
+		isAuthenticated: null,
+		loading: true,
+		error: null,
+		user: null,
+	};
 
-  const [state, dispatch] = useReducer(AuthReducer, initialState);
-  //load user
-  //register user
-  // login user
-  // logout
-  // clear errors
-  return (
-    <AuthContext.Provider
-      value={{
-        token: state.token,
-        isAuthenticated: state.isAuthenticated,
-        loading: state.loading,
-        error: state.error,
-        user: state.user,
-      }}
-    >
-      {props.children}
-    </AuthContext.Provider>
-  );
+	const [state, dispatch] = useReducer(AuthReducer, initialState);
+	//load user
+	const loadUser = () => {
+		console.log('Login User');
+	};
+	//register user
+	const register = async (formData) => {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+		try {
+			const res = await Axios.post('/api/users', formData, config);
+			dispatch({ type: REGISTER_SUCCESS, payload: res.data });
+		} catch (err) {
+			dispatch({ type: REGISTER_FAIL, payload: err.res.data.msg });
+		}
+	};
+	// login user
+	const loginUser = () => {
+		console.log('Login User');
+	};
+	// logout
+	const logoutUser = () => {
+		console.log('Login User');
+	};
+	// clear errors
+	const clearError = () => {
+		dispatch({ type: CLEAR_ERRORS });
+	};
+	return (
+		<AuthContext.Provider
+			value={{
+				token: state.token,
+				isAuthenticated: state.isAuthenticated,
+				loading: state.loading,
+				error: state.error,
+				user: state.user,
+				register,
+				loadUser,
+				loginUser,
+				logoutUser,
+				clearError,
+			}}
+		>
+			{props.children}
+		</AuthContext.Provider>
+	);
 };
 export default AuthState;
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,82 +1,96 @@
-import React, { useContext, useState } from "react";
-import AlertContext from "../../Context/alert/alertContext";
+import React, { useContext, useState, useEffect } from 'react';
+import AlertContext from '../../Context/alert/alertContext';
+import AuthContext from '../../Context/auth/authContext';
 const Register = () => {
-  const alertContext = useContext(AlertContext);
-  const { setAlert } = alertContext;
-  const [user, setUser] = useState({
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
-  });
-  const { name, email, password, password2 } = user;
-  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
-  const onSubmit = (e) => {
-    e.preventDefault();
-    if (name === "" || email === "" || password === "") {
-      setAlert("please input all the field", "danger");
-    } else if (password !== password2) {
-      setAlert("password not same", "danger");
-    } else {
-      console.log("Register Submit");
-    }
-  };
-  return (
-    <div className="form-container">
-      <h1>
-        account <span className="text-primary">register</span>
-      </h1>
-      <form onSubmit={onSubmit}>
-        <div className="form-group">
-          <label htmlFor="name">Name</label>
-          <input
-            type="text"
-            name="name"
-            value={name}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">email</label>
-          <input
-            type="email"
-            name="email"
-            value={email}
-            onChange={onChange}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">password</label>
-          <input
-            type="password"
-            name="password"
-            value={password}
-            onChange={onChange}
-            required
-            minLength="6"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password2">confirm password</label>
-          <input
-            type="password"
-            name="password2"
-            value={password2}
-            onChange={onChange}
-            required
-            minLength="6"
-          />
-        </div>
-        <input
-          type="submit"
-          value="register"
-          className="btn btn-primary btn-block"
-        />
-      </form>
-    </div>
-  );
+	const alertContext = useContext(AlertContext);
+	const authContext = useContext(AuthContext);
+	const { setAlert } = alertContext;
+	const { register, error, clearError } = authContext;
+	useEffect(() => {
+		if (error === 'User already exists') {
+			setAlert(error, 'danger');
+			clearError();
+		}
+	}, [error]);
+	const [user, setUser] = useState({
+		name: '',
+		email: '',
+		password: '',
+		password2: '',
+	});
+	const { name, email, password, password2 } = user;
+	const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+	const onSubmit = (e) => {
+		e.preventDefault();
+		if (name === '' || email === '' || password === '') {
+			setAlert('please input all the field', 'danger');
+		} else if (password !== password2) {
+			setAlert('password not same', 'danger');
+		} else {
+			register({
+				name,
+				email,
+				password,
+			});
+			console.log('Register Submit');
+		}
+	};
+	return (
+		<div className='form-container'>
+			<h1>
+				account <span className='text-primary'>register</span>
+			</h1>
+			<form onSubmit={onSubmit}>
+				<div className='form-group'>
+					<label htmlFor='name'>Name</label>
+					<input
+						type='text'
+						name='name'
+						value={name}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<label htmlFor='email'>email</label>
+					<input
+						type='email'
+						name='email'
+						value={email}
+						onChange={onChange}
+						required
+					/>
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password'>password</label>
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={onChange}
+						required
+						minLength='6'
+					/>
+				</div>
+				<div className='form-group'>
+					<label htmlFor='password2'>confirm password</label>
+					<input
+						type='password'
+						name='password2'
+						value={password2}
+						onChange={onChange}
+						required
+						minLength='6'
+					/>
+				</div>
+				<input
+					type='submit'
+					value='register'
+					className='btn btn-primary btn-block'
+				/>
+			</form>
+		</div>
+	);
 };
 
 export default Register;
