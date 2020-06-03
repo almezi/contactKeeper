47f22047d38638db93c2786e49bb7ed6e3ff77af
auliameiazizi@gmail.com <github_2sembilan>
2020-06-04 01:04:45 +0700

    Fix Errors in Useeffect in Home.pages.js

--- client/src/components/pages/Home.pages.js
+++ client/src/components/pages/Home.pages.js
@@ -1,25 +1,25 @@
-import React, { useContextS, useEffect } from 'react';
-import Contacts from '../contacts/Contacts';
-import ContactForm from '../contacts/ContactForm';
-import ContactFilter from '../contacts/contactFilter';
-import AuthContext from '../../Context/auth/authContext';
+import React, { useContext, useEffect } from "react";
+import Contacts from "../contacts/Contacts";
+import ContactForm from "../contacts/ContactForm";
+import ContactFilter from "../contacts/contactFilter";
+import AuthContext from "../../Context/auth/authContext";
 const Home = () => {
-	const authContext = useContext(AuthContext);
-	useEffect(() => {
-		authContext.loadUser;
-		//eslint-disable-next-line
-	}, []);
-	return (
-		<div className='grid-2'>
-			<div>
-				<ContactForm />
-			</div>
-			<div>
-				<ContactFilter />
-				<Contacts />
-			</div>
-		</div>
-	);
+  const authContext = useContext(AuthContext);
+  useEffect(() => {
+    authContext.loadUser();
+    //eslint-disable-next-line
+  }, []);
+  return (
+    <div className="grid-2">
+      <div>
+        <ContactForm />
+      </div>
+      <div>
+        <ContactFilter />
+        <Contacts />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
